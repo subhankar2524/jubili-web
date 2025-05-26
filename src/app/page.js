9936a59d659b35b9d48b65fb081577dbd53bdab5
subhankar <subhankarg2524@gmail.com>
2025-05-26 14:23:37 +0530

    delete all

--- src/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <p className="text-3xl font-bold">Home page</p>
-      <p>Display User info here after login</p>
-    </div>
-  );
-}
